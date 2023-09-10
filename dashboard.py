8a28bb4d278599e4675030ee1e975fffe2d21d61
Tobias Byland <36913362+kelloggs13@users.noreply.github.com>
2023-09-10 19:26:04 +0200

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+from pycaret.datasets import get_data
+data = get_data('diabetes')[1:100]
+
+# import pycaret classification and init setup
+from pycaret.classification import *
+s = setup(data, target = 'Class variable', session_id = 123)
+
+# import ClassificationExperiment and init the class
+from pycaret.classification import ClassificationExperiment
+exp = ClassificationExperiment()
+
+# init setup on exp
+exp.setup(data, target = 'Class variable', session_id = 123)
+
+# Create a progress bar
+progress_bar = st.progress(0)
+
+# compare baseline models
+best = compare_models()
+
+# Update the progress bar when appropriate
+progress_bar.progress(50)  # Set to 100% when done
+
+st.write(best)
+
