83d03c57460f3626b0fb02c8324785e33138f922
Tobias Byland <36913362+kelloggs13@users.noreply.github.com>
2023-09-17 17:05:59 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,21 +1,21 @@
 # load the dataset from pycaret
 from pycaret.datasets import get_data
-data = get_data('diamond')
+data = get_data("diamond")
 # initialize setup
 from pycaret.regression import *
-s = setup(data, target = 'Price', transform_target = True, log_experiment = True, experiment_name = 'diamond')
+s = setup(data, target = "Price", transform_target = True, log_experiment = True, experiment_name = "diamond")
 # compare all models
 best = compare_models()
 # check the final params of best model
 best.get_params()
 # check the residuals of trained model
-plot_model(best, plot = 'residuals_interactive')
-plot_model(best, plot = ‘feature’)
+plot_model(best, plot = "residuals_interactive")
+plot_model(best, plot = "feature")
 evaluate_model(best)
 
 # copy data and remove target variable
 data_unseen = data.copy()
-data_unseen.drop(‘Price’, axis = 1, inplace = True)
+data_unseen.drop("Price", axis = 1, inplace = True)
 predictions = predict_model(best, data = data_unseen)
 
 
